a0d7f0a9a2a030dbedec75042ac095c1df25086e
Npirela <93156380+Npirela@users.noreply.github.com>
2024-12-15 16:12:07 -0300

    various changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Script para cambiar el contador de pacientes de manera diaria por 5 pacientes que se atienden en la consulta 
 // Cantidad inicial
-let CantidadPacientesContador = 4023; // Cambia este valor a tu cantidad específica
+let CantidadPacientesContador = 10000; // Cambia este valor a tu cantidad específica
 let incremento = 2;
 
 // Obtener datos del almacenamiento local
