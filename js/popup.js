342ce9c238653f7558da87600371ef70c1d2b16d
Npirela <93156380+Npirela@users.noreply.github.com>
2025-06-11 18:07:41 -0300

    add pop up dia del padre

---
+++ js/popup.js
@@ -0,0 +1,5 @@
+/*usando modals para mostrar imagen como pop up*/
+
+const myModal = bootstrap.Modal.getOrCreateInstance('#ad-popup');
+
+window.addEventListener('DOMContentLoaded', () => {myModal.show();});
